96ce9e8f57c736acec36bb38a49ca8a9a58ab362
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-01 13:58:39 +0300

    add base js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const modal = document.querySelector('.mobile-menu-modal-box');
 const burger = document.querySelector('.burger-button');
+const closeBtn = document.querySelector('.mobile-menu-close-button');
+const menuNavLinks = document.querySelectorAll('.mobile-menu-modal-box-link');
 
 burger.addEventListener('click', () => {
   modal.classList.add('is-open');
+});
+
+function closeModal() {
+  modal.classList.remove('is-open');
+}
+
+closeBtn.addEventListener('click', closeModal);
+menuNavLinks.forEach(link => {
+  link.addEventListener('click', closeModal);
 });
