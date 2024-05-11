6d21311f6c13131b9b349493c69f9bf370a847bf
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-11 22:56:13 +0300

    add js to hero btn / make it open and close the modal

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
+// ================================= OPENING AND CLOSING FNS
+
+const html = document.querySelector('html');
+
+function openModal(element) {
+  element.classList.add('is-open');
+  html.classList.add('no-scroll');
+}
+
+function closeModal(element) {
+  element.classList.remove('is-open');
+  html.classList.remove('no-scroll');
+}
+
+// ================================== MENU
 const modal = document.querySelector('.mobile-menu-modal-box');
 const burger = document.querySelector('.burger-button');
 const closeBtn = document.querySelector('.mobile-menu-close-button');
 const menuNavLinks = document.querySelectorAll('.mobile-menu-modal-box-link');
 
-burger.addEventListener('click', () => {
-  modal.classList.add('is-open');
-});
+burger.addEventListener('click', () => openModal(modal));
+closeBtn.addEventListener('click', () => closeModal(modal));
 
-function closeModal() {
-  modal.classList.remove('is-open');
-}
-
-closeBtn.addEventListener('click', closeModal);
 menuNavLinks.forEach(link => {
-  link.addEventListener('click', closeModal);
+  link.addEventListener('click', () => closeModal(modal));
 });
+
+// ================================== FORM
+const formModal = document.querySelector('.backdrop');
+const formCloseBtn = document.querySelector('.close-button');
+const heroBtn = document.querySelector('.hero-button');
+
+heroBtn.addEventListener('click', () => openModal(formModal));
+formCloseBtn.addEventListener('click', () => closeModal(formModal));
